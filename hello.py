d132f3dee9c97dfdab67ba0c5985ef25e1dcf3ce
mingkai <mingkai.lee@gmail.com>
2015-12-02 11:35:44 +0800

    Initial changes to hello.py

--- hello.py
+++ hello.py
@@ -1,32 +1,42 @@
 import os
 import uuid
+import socket
+
 from flask import Flask
 
 
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
+RED = "#FF0000"
 GREEN = "#33CC33"
 
-COLOR = GREEN
+COLOR = RED
+count = 0
 
 @app.route('/')
 def hello():
-
-
+    global count
+    count += 1
+    if count > 10:
+	extra = '<h2>Are you so bored to click this '+str(count)+' times???... Get a life!!!</h2>'
+    else:
+	extra = ''
     return """
     <html>
     <body bgcolor="{}">
 
     <center><h1><font color="white">Hi, I'm GUID:<br/>
-    {}</br>
+    {} on host {}</br>
+    <h2>Count is : {} </h2>
+    {}
 
 
     </center>
 
     </body>
     </html>
-    """.format(COLOR,my_uuid,)
+    """.format(COLOR,my_uuid,socket.gethostname(),count,extra)
 
 if __name__ == "__main__":
 	app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
